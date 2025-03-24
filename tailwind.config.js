caa7e08aad39967b48a6bee3d8821d9904b850a0
Sakacogu <sarakatrin772@gmail.com>
2025-03-24 14:49:10 +0000

    reyna að losa mig við dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    darkMode: "class",
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Poppins", "ui-sans-serif", "system-ui"],
-                serif: ["Playfair Display", "ui-serif", "Georgia"],
-        },
-    },
-    plugins: [],
-}
+export default {
+  content: ["./**/*.{html,js}"],
+  blocklist: [
+    "\[default\:\\u202F\$\{N\(\`\$\{u\}\`\)\}\]"  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
