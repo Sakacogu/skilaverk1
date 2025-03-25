5a328037ebf0f59c1c89fd5df4f8c89cfe2a4cd6
Sakacogu <sarakatrin772@gmail.com>
2025-03-25 13:31:28 +0000

    færa JS i sér folder

---
+++ src/menuScript.js
@@ -0,0 +1,9 @@
+
+  window.addEventListener('scroll', function() {
+    const scrollToTopButton = document.getElementById('scrollToTopButton');
+    if (window.scrollY > 100) {
+      scrollToTopButton.classList.remove('hidden');
+    } else {
+      scrollToTopButton.classList.add('hidden');
+    }
+  });
